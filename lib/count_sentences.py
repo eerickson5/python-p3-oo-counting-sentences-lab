8132d12dda762bbbb6c9e21ad1b62853f67de899
Elise Erickson <elisece717@gmail.com>
2024-02-07 16:32:38 -0500

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
+import ipdb
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self.value.endswith(".") and len(self.value) > 1
+  
+  def is_question(self):
+    return self.value.endswith("?") and len(self.value) > 1
+  
+  def is_exclamation(self):
+    return self.value.endswith("!") and len(self.value) > 1
+  
+  def count_sentences(self):
+    question_version = self.value.replace("!", "?")
+    question_version = question_version.replace(".", "?")
+
+    array_version = question_version.split("?")
+    count = 0
+    # ipdb.set_trace()
+    for string in array_version:
+      if len(string) > 1:
+        count += 1
+    return count
+  
